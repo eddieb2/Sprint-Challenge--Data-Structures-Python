5caf9ee4f7c0ad44e4e5c4c4d970a77be141efca
Edward Blanciak <blanciak.edward2@gmail.com>
2020-08-14 11:11:00 -0400

    sprint started

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.index = 0
+        self.data = []
 
     def append(self, item):
-        pass
+        # The append method adds the given element to the buffer.
+        # if data is not full, append data to the list
+        if self.data < self.capacity:
+            pass
+
+        # if data is full, replace oldest value with new value
+        if self.data == self.capacity:
+            pass
 
     def get(self):
+        # The get method returns all of the elements in the buffer
+        # in a list in their given order. It should not return any None values
+        # in the list even if they are present in the ring buffer.
         pass
